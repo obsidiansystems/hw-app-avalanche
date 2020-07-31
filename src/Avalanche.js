4f76bcb8e5d7701cbcda3a42020727bfe5e52df0
Tom Smalley <tfsmalley@gmail.com>
2020-07-31 14:29:57 +0100

    Initial commit for Avalanche

---
+++ src/Avalanche.js
@@ -0,0 +1,156 @@
+//@flow
+
+import type Transport from "@ledgerhq/hw-transport";
+import BIPPath from "bip32-path";
+
+/**
+ * Nervos API
+ *
+ * @example
+ * import Avalanche from "@ledgerhq/hw-app-avalanche";
+ * const avalanche = new Avalanche(transport);
+ */
+export default class Avalanche {
+  transport: Transport<*>;
+
+  constructor(transport: Transport<*>, scrambleKey: string = "Avalanche") {
+    this.transport = transport;
+    transport.decorateAppAPIMethods(
+      this,
+      [
+        "getAppConfiguration",
+        "getWalletId",
+        "getWalletPublicKey",
+        "signTransaction"
+      ],
+      scrambleKey
+    );
+  }
+
+  /**
+   * get Avalanche address for a given BIP 32 path.
+   *
+   * @param path a path in BIP 32 format
+   * @return an object with a publicKey
+   * @example
+   * const result = await avalanche.getWalletPublicKey("44'/9000'/0'/0/0");
+   * const publicKey = result;
+   */
+  async getWalletPublicKey(path: string): Promise<string> {
+    const bipPath = BIPPath.fromString(path).toPathArray();
+
+    const cla = 0x80;
+    const ins = 0x02;
+    const p1 = 0x00;
+    const p2 = 0x00;
+    const data = Buffer.alloc(1 + bipPath.length * 4);
+
+    data.writeUInt8(bipPath.length, 0);
+    bipPath.forEach((segment, index) => {
+      data.writeUInt32BE(segment, 1 + index * 4);
+    });
+
+    const response = await this.transport.send(cla, ins, p1, p2, data);
+    const publicKeyLength = response[0];
+    return response.slice(1, 1 + publicKeyLength).toString("hex");
+  }
+
+  /**
+   * Sign a Nervos transaction with a given BIP 32 path
+   *
+   * @param path a path in BIP 32 format
+   * @param rawTxHex transaction to sign
+   * @param contextTransaction list of transaction context to use in parsing (optional)
+   * @return a signature as hex string
+   * @example
+   * const signature = await avalanche.signTransaction("44'/9000'/0'/0/0", "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+   */
+  async signTransaction(
+    path: string,
+    rawTxHex: string,
+    rawContextsTxHex: [string]
+  ): Promise<string> {
+    const bipPath = BIPPath.fromString(path).toPathArray();
+    const rawTx = Buffer.from(rawTxHex, "hex");
+
+    let rawPath = Buffer.alloc(1 + bipPath.length * 4);
+    rawPath.writeInt8(bipPath.length, 0);
+    bipPath.forEach((segment, index) => {
+      rawPath.writeUInt32BE(segment, 1 + index * 4);
+    });
+    await this.transport.send(0x80, 0x03, 0x00, 0x00, rawPath);
+
+    const maxApduSize = 230;
+
+    if (!rawContextsTxHex) rawContextsTxHex = [];
+
+    for (const rawContextTxHex of rawContextsTxHex) {
+      let rawContextTx =
+        rawContextTxHex !== null ? Buffer.from(rawContextTxHex, "hex") : null;
+      let rawContextFullChunks = Math.floor(rawContextTx.length / maxApduSize);
+      for (let i = 0; i < rawContextFullChunks; i++) {
+        let data = rawContextTx.slice(i*maxApduSize, (i+1)*maxApduSize);
+        await this.transport.send(0x80, 0x03, 0x21, 0x00, data);
+      }
+
+      let lastContextOffset = Math.floor(rawContextTx.length / maxApduSize) * maxApduSize;
+      let lastContextData = rawContextTx.slice(lastContextOffset, lastContextOffset+maxApduSize);
+      await this.transport.send(0x80, 0x03, 0xa1, 0x00, lastContextData);
+    }
+
+    let txFullChunks = Math.floor(rawTx.length / maxApduSize);
+    for (let i = 0; i < txFullChunks; i++) {
+      let data = rawTx.slice(i*maxApduSize, (i+1)*maxApduSize);
+      await this.transport.send(0x80, 0x03, 0x01, 0x00, data);
+    }
+
+    let lastOffset = Math.floor(rawTx.length / maxApduSize) * maxApduSize;
+    let lastData = rawTx.slice(lastOffset, lastOffset+maxApduSize);
+    let response = await this.transport.send(0x80, 0x03, 0x81, 0x00, lastData);
+    return response.toString("hex");
+  }
+
+  /**
+   * Get the version of the Nervos app installed on the hardware device
+   *
+   * @return an object with a version
+   * @example
+   * const result = await avalanche.getAppConfiguration();
+   *
+   * {
+   *   "version": "1.0.3",
+   *   "hash": "0000000000000000000000000000000000000000"
+   * }
+   */
+  async getAppConfiguration(): Promise<{
+    version: string,
+    hash: string
+  }> {
+    const response1 = await this.transport.send(0x80, 0x00, 0x00, 0x00);
+    const response2 = await this.transport.send(0x80, 0x09, 0x00, 0x00);
+
+    const result = {};
+    result.version =
+      "" + response1[0] + "." + response1[1] + "." + response1[2];
+    result.hash = response2.toString("hex");
+
+    return result;
+  }
+
+  /**
+   * Get the wallet identifier for the Ledger wallet
+   *
+   * @return a byte string
+   * @example
+   * const id = await avalanche.getWalletId();
+   *
+   * "0x69c46b6dd072a2693378ef4f5f35dcd82f826dc1fdcc891255db5870f54b06e6"
+   */
+  async getWalletId(): Promise<string> {
+    const response = await this.transport.send(0x80, 0x01, 0x00, 0x00);
+
+    const result = response.slice(0, 32).toString("hex");
+
+    return result;
+  }
+}
---
+++ tests/Avalanche.test.js
@@ -0,0 +1,73 @@
+import {
+  createTransportReplayer,
+  RecordStore
+} from "@ledgerhq/hw-transport-mocker";
+import Avalanche from "../src/Avalanche";
+
+test("avalanche.getPublicKey", async () => {
+  const Transport = createTransportReplayer(
+    RecordStore.fromString(`
+      => 8002000015058000002c80000135800000000000000100000000
+      <= 4104d066dbe5603004dc4c83e27106b098f5e9b9b17b6bdec965810cd0921193b1c87206518153a0b44c7ae6b0be92a60d515d454e71ab27943bbb323273b8bdd46a9000
+    `)
+  );
+  const transport = await Transport.open();
+  const avalanche = new Avalanche(transport);
+  const result = await avalanche.getWalletPublicKey("m/44'/9000'/0'/1/0");
+});
+
+test("avalanche.getAppConfiguration", async () => {
+  const Transport = createTransportReplayer(
+    RecordStore.fromString(`
+    => 8000000000
+    <= 0001009000
+    => 8009000000
+    <= 0001009000
+    `)
+  );
+  const transport = await Transport.open();
+  const avalanche = new Avalanche(transport);
+  var configuration = await avalanche.getAppConfiguration();
+  expect(configuration).toEqual({
+    version: "0.1.0",
+    hash: "0001009000",
+  });
+});
+
+test("avalanche.getWalletId", async () => {
+  const Transport = createTransportReplayer(
+    RecordStore.fromString(`
+    => 8001000000
+    <= 27fe5acb022cd7b8be0eb7009d42ff4600c597d28b6affefcab6f7396d1311c2f58fc334be619099b733be1da93f26b674bd08d110b39f6d39b6cf6aa01618a59000
+    `)
+  );
+
+  const transport = await Transport.open();
+
+  const avalanche = new Avalanche(transport);
+
+  const result = await avalanche.getWalletId();
+  expect(result).toEqual("27fe5acb022cd7b8be0eb7009d42ff4600c597d28b6affefcab6f7396d1311c2");
+});
+
+test("avalanche.signTransaction", async () => {
+  const Transport = createTransportReplayer(
+    RecordStore.fromString(`
+    => 8003000015058000002c80000135800000000000000100000000
+    <= 00009000
+    => 80038100e60030303030303030303030303030303030303030303030303030303030303030303030303030303030000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+    <= 314402205eac720be544d3959a760d9bfd6a0e7c86d128fd1030038f06d85822608804e20220385d83273c9d03c469596292fb354b07d193034f83c2633a4c1f057838e12a5b9000
+    `)
+  );
+
+  const transport = await Transport.open();
+
+  const avalanche = new Avalanche(transport);
+
+  const result = await avalanche.signTransaction(
+    "44'/9000'/0'/1/0",
+    Buffer.from("8000002c80000135800000000000000100000000").toString("hex")
+  );
+
+  expect(result).toEqual("314402205eac720be544d3959a760d9bfd6a0e7c86d128fd1030038f06d85822608804e20220385d83273c9d03c469596292fb354b07d193034f83c2633a4c1f057838e12a5b9000");
+});
