8a13bb2ec0eaacb0dc2e0607e6c76aaca9f196bf
Elliot Cameron <eacameron@gmail.com>
2020-08-05 18:20:16 -0400

    Convert signTransaction to signHash

--- src/Avalanche.js
+++ src/Avalanche.js
@@ -13,6 +13,15 @@ import BIPPath from "bip32-path";
 export default class Avalanche {
   transport: Transport<*>;
 
+  MAX_APDU_SIZE = 230;
+
+  INS_VERSION = 0x00;
+  INS_GIT = 0x01;
+  INS_GET_WALLET_ID = 0x02;
+  INS_PROMPT_PUBLIC_KEY = 0x03;
+  INS_PROMPT_EXT_PUBLIC_KEY = 0x04;
+  INS_SIGN_HASH = 0x05;
+
   constructor(transport: Transport<*>, scrambleKey: string = "Avalanche") {
     this.transport = transport;
     transport.decorateAppAPIMethods(
@@ -28,7 +37,7 @@ export default class Avalanche {
   }
 
   /**
-   * get Avalanche address for a given BIP 32 path.
+   * get Avalanche address for a given BIP-32 path.
    *
    * @param path a path in BIP 32 format
    * @return an object with a publicKey
@@ -40,7 +49,7 @@ export default class Avalanche {
     const bipPath = BIPPath.fromString(path).toPathArray();
 
     const cla = 0x80;
-    const ins = 0x02;
+    const ins = this.INS_PROMPT_PUBLIC_KEY;
     const p1 = 0x00;
     const p2 = 0x00;
     const data = Buffer.alloc(1 + bipPath.length * 4);
@@ -68,7 +77,7 @@ export default class Avalanche {
     const bipPath = BIPPath.fromString(path).toPathArray();
 
     const cla = 0x80;
-    const ins = 0x04;
+    const ins = this.INS_PROMPT_EXT_PUBLIC_KEY;
     const p1 = 0x00;
     const p2 = 0x00;
     const data = Buffer.alloc(1 + bipPath.length * 4);
@@ -89,57 +98,38 @@ export default class Avalanche {
   }
 
   /**
-   * Sign an Avalanche transaction with a given BIP 32 path
+   * Sign a hash with a given BIP-32 path.
    *
    * @param path a path in BIP 32 format
-   * @param rawTxHex transaction to sign
-   * @param contextTransaction list of transaction context to use in parsing (optional)
+   * @param hash hex-encoded hash to sign
    * @return a signature as hex string
    * @example
-   * const signature = await avalanche.signTransaction("44'/9000'/0'/0/0", "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+   * const signature = await avalanche.signHash("44'/9000'/0'/0/0", "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
    */
-  async signTransaction(
+  async signHash(
     path: string,
-    rawTxHex: string,
-    rawContextsTxHex: [string]
+    hash: string
   ): Promise<string> {
     const bipPath = BIPPath.fromString(path).toPathArray();
-    const rawTx = Buffer.from(rawTxHex, "hex");
+    const rawTx = Buffer.from(hash, "hex");
 
     let rawPath = Buffer.alloc(1 + bipPath.length * 4);
     rawPath.writeInt8(bipPath.length, 0);
     bipPath.forEach((segment, index) => {
       rawPath.writeUInt32BE(segment, 1 + index * 4);
     });
-    await this.transport.send(0x80, 0x03, 0x00, 0x00, rawPath);
+    await this.transport.send(0x80, this.INS_SIGN_HASH, 0x00, 0x00, rawPath);
 
-    const maxApduSize = 230;
-
-    if (!rawContextsTxHex) rawContextsTxHex = [];
-
-    for (const rawContextTxHex of rawContextsTxHex) {
-      let rawContextTx =
-        rawContextTxHex !== null ? Buffer.from(rawContextTxHex, "hex") : null;
-      let rawContextFullChunks = Math.floor(rawContextTx.length / maxApduSize);
-      for (let i = 0; i < rawContextFullChunks; i++) {
-        let data = rawContextTx.slice(i*maxApduSize, (i+1)*maxApduSize);
-        await this.transport.send(0x80, 0x03, 0x21, 0x00, data);
-      }
-
-      let lastContextOffset = Math.floor(rawContextTx.length / maxApduSize) * maxApduSize;
-      let lastContextData = rawContextTx.slice(lastContextOffset, lastContextOffset+maxApduSize);
-      await this.transport.send(0x80, 0x03, 0xa1, 0x00, lastContextData);
-    }
-
-    let txFullChunks = Math.floor(rawTx.length / maxApduSize);
+    let txFullChunks = Math.floor(rawTx.length / this.MAX_APDU_SIZE);
     for (let i = 0; i < txFullChunks; i++) {
-      let data = rawTx.slice(i*maxApduSize, (i+1)*maxApduSize);
-      await this.transport.send(0x80, 0x03, 0x01, 0x00, data);
+      let data = rawTx.slice(i*this.MAX_APDU_SIZE, (i+1)*this.MAX_APDU_SIZE);
+      await this.transport.send(0x80, this.INS_SIGN_HASH, 0x01, 0x00, data);
     }
 
-    let lastOffset = Math.floor(rawTx.length / maxApduSize) * maxApduSize;
-    let lastData = rawTx.slice(lastOffset, lastOffset+maxApduSize);
-    let response = await this.transport.send(0x80, 0x03, 0x81, 0x00, lastData);
+    let lastOffset = Math.floor(rawTx.length / this.MAX_APDU_SIZE) * this.MAX_APDU_SIZE;
+    let lastData = rawTx.slice(lastOffset, lastOffset+this.MAX_APDU_SIZE);
+    let response = await this.transport.send(0x80, this.INS_SIGN_HASH, 0x81, 0x00, lastData);
+    // TODO: This includes the 9000 at the end and the 32-byte hash at the beginning.
     return response.toString("hex");
   }
 
@@ -159,13 +149,13 @@ export default class Avalanche {
     version: string,
     hash: string
   }> {
-    const response1 = await this.transport.send(0x80, 0x00, 0x00, 0x00);
-    const response2 = await this.transport.send(0x80, 0x09, 0x00, 0x00);
+    const version = await this.transport.send(0x80, this.INS_VERSION, 0x00, 0x00);
+    const hash = await this.transport.send(0x80, this.INS_GIT, 0x00, 0x00);
 
     const result = {};
     result.version =
-      "" + response1[0] + "." + response1[1] + "." + response1[2];
-    result.hash = response2.toString("hex");
+      "" + version[0] + "." + version[1] + "." + version[2];
+    result.hash = hash.toString("hex");
 
     return result;
   }
@@ -180,7 +170,7 @@ export default class Avalanche {
    * "0x69c46b6dd072a2693378ef4f5f35dcd82f826dc1fdcc891255db5870f54b06e6"
    */
   async getWalletId(): Promise<string> {
-    const response = await this.transport.send(0x80, 0x01, 0x00, 0x00);
+    const response = await this.transport.send(0x80, this.INS_GET_WALLET_ID, 0x00, 0x00);
 
     const result = response.slice(0, 32).toString("hex");
 
