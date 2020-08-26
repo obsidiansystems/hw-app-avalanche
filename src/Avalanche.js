cbc215b8cbedc136130eefc85e5057e8965b1a46
Elliot Cameron <eacameron@gmail.com>
2020-08-26 16:21:58 -0400

    Port changes for Everest PKH and multi-sign protocol

--- src/Avalanche.js
+++ src/Avalanche.js
@@ -12,8 +12,11 @@ import BIPPath from "bip32-path";
  */
 export default class Avalanche {
   transport: Transport<*>;
+  logger: (msg: string) => undefined;
 
+  CLA = 0x80;
   MAX_APDU_SIZE = 230;
+  MAX_HRP_LENGTH = 24;
 
   INS_VERSION = 0x00;
   INS_GET_WALLET_ID = 0x01;
@@ -21,15 +24,20 @@ export default class Avalanche {
   INS_PROMPT_EXT_PUBLIC_KEY = 0x03;
   INS_SIGN_HASH = 0x04;
 
-  constructor(transport: Transport<*>, scrambleKey: string = "Avalanche") {
+  constructor(
+    transport: Transport<*>,
+    scrambleKey: string = "Avalanche",
+    logger: (msg: string) => undefined = console.error,
+  ) {
     this.transport = transport;
+    this.logger = logger;
     transport.decorateAppAPIMethods(
       this,
       [
         "getAppConfiguration",
         "getWalletId",
         "getWalletPublicKey",
-        "signTransaction"
+        "signHash"
       ],
       scrambleKey
     );
@@ -43,49 +51,41 @@ export default class Avalanche {
    * @example
    * await avalanche.getWalletPublicKey("44'/9000'/0'/0/0");
    */
-  async getWalletPublicKey(derivation_path: string): Promise<Buffer> {
-    const bipPath = BIPPath.fromString(derivation_path).toPathArray();
+  async getWalletAddress(derivation_path: string, hrp = ""): Promise<Buffer> {
+    if (hrp.length > this.MAX_HRP_LENGTH) {
+      throw "Maximum Bech32 'human readable part' length exceeded";
+    }
 
-    const cla = 0x80;
+    const cla = this.CLA;
     const ins = this.INS_PROMPT_PUBLIC_KEY;
-    const p1 = 0x00;
+    const p1 = hrp.length;
     const p2 = 0x00;
-    const data = Buffer.alloc(1 + bipPath.length * 4);
-
-    data.writeUInt8(bipPath.length, 0);
-    bipPath.forEach((segment, index) => {
-      data.writeUInt32BE(segment, 1 + index * 4);
-    });
+    const data = Buffer.concat([
+      Buffer.from(hrp, "latin1"),
+      this.encodeBip32Path(BIPPath.fromString(derivation_path)),
+    ]);
 
     const response = await this.transport.send(cla, ins, p1, p2, data);
-    const publicKeyLength = response[0];
-    return response.slice(1, 1 + publicKeyLength);
+    return response.slice(0, -2);
   }
 
   /**
-   * get extended public key for a given BIP 32 path.
+   * get extended public key for a given BIP-32 path.
    *
-   * @param derivation_path a path in BIP 32 format
+   * @param derivation_path a path in BIP-32 format
    * @return an object with a buffer for the public key data and a buffer for the chain code
    * @example
    * await avalanche.getWalletExtendedPublicKey("44'/9000'/0'/0/0");
    */
   async getWalletExtendedPublicKey(derivation_path: string): Promise<{
     public_key: Buffer,
-    chain_code: Buffer
+    chain_code: Buffer,
   }> {
-    const bipPath = BIPPath.fromString(derivation_path).toPathArray();
-
-    const cla = 0x80;
+    const cla = this.CLA;
     const ins = this.INS_PROMPT_EXT_PUBLIC_KEY;
     const p1 = 0x00;
     const p2 = 0x00;
-    const data = Buffer.alloc(1 + bipPath.length * 4);
-
-    data.writeUInt8(bipPath.length, 0);
-    bipPath.forEach((segment, index) => {
-      data.writeUInt32BE(segment, 1 + index * 4);
-    });
+    const data: Buffer = this.encodeBip32Path(BIPPath.fromString(derivation_path));
 
     const response = await this.transport.send(cla, ins, p1, p2, data);
     const publicKeyLength = response[0];
@@ -100,50 +100,46 @@ export default class Avalanche {
   /**
    * Sign a hash with a given BIP-32 path.
    *
-   * @param derivation_path a path in BIP-32 format
-   * @param hash 32-byte buffer containing hash of a transaction
-   * @return a buffer with the signature data
+   * @param derivationPathPrefix a BIP-32 path that will act as the prefix to all other signing paths.
+   * @param derivationPathSuffixes an array of BIP-32 path suffixes that will be
+   *                               appended to the prefix to form the final path for signing.
+   * @param hash 32-byte buffer containing the hash to sign
+   * @return a map of path suffixes (as strings) to signature buffers
    * @example
-   * await avalanche.signHash(
-   *   "44'/9000'/0'/0/0",
-   *   Buffer.from("0000000000000000000000000000000000000000000000000000000000000000", "hex")
-   * );
+   * const signatures = await avalanche.signHash(
+   *   BIPPath.fromString("44'/9000'/0'"),
+   *   [BIPPath.fromString("0/0")],
+   *   Buffer.from("0000000000000000000000000000000000000000000000000000000000000000", "hex"));
    */
   async signHash(
-    derivation_path: string,
+    derivationPathPrefix: BIPPath,
+    derivationPathSuffixes: Array<BIPPath>,
     hash: Buffer,
-  ): Promise<{
-    hash: Buffer,
-    signature: Buffer
-  }> {
-    const bipPath = BIPPath.fromString(derivation_path).toPathArray();
-
-    let rawPath = Buffer.alloc(1 + bipPath.length * 4);
-    rawPath.writeInt8(bipPath.length, 0);
-    bipPath.forEach((segment, index) => {
-      rawPath.writeUInt32BE(segment, 1 + index * 4);
-    });
-    await this.transport.send(0x80, this.INS_SIGN_HASH, 0x00, 0x00, rawPath);
-
-    const txFullChunks = Math.floor(hash.length / this.MAX_APDU_SIZE);
-    for (let i = 0; i < txFullChunks; i++) {
-      const data = hash.slice(i*this.MAX_APDU_SIZE, (i+1)*this.MAX_APDU_SIZE);
-      await this.transport.send(0x80, this.INS_SIGN_HASH, 0x01, 0x00, data);
+  ): Promise<Map<string, Buffer>> {
+    if (hash.length != 32) {
+      throw "Hash buffer must be 32 bytes";
     }
 
-    const lastOffset = Math.floor(hash.length / this.MAX_APDU_SIZE) * this.MAX_APDU_SIZE;
-    const lastData = hash.slice(lastOffset, lastOffset+this.MAX_APDU_SIZE);
-    const response = await this.transport.send(0x80, this.INS_SIGN_HASH, 0x81, 0x00, lastData);
-
-    const responseHash = response.slice(0, 32);
-    if (!responseHash.equals(hash)) {
-      throw "Signed hash does not match input hash!";
+    const firstMessage: Buffer = Buffer.concat([
+      this.uInt8Buffer(derivationPathSuffixes.length),
+      hash,
+      this.encodeBip32Path(derivationPathPrefix)
+    ]);
+    const responseHash = await this.transport.send(this.CLA, this.INS_SIGN_HASH, 0x00, 0x00, firstMessage);
+    if (!responseHash.slice(0, 32).equals(hash)) {
+      throw "Ledger reported a hash that does not match the input hash!";
     }
 
-    return {
-      hash: responseHash,
-      signature: response.slice(32, -2),
+    let resultMap: Map<string, Buffer> = new Map();
+    for (let ix = 0; ix < derivationPathSuffixes.length; ix++) {
+      const suffix = derivationPathSuffixes[ix];
+      this.logger("Signing with " + suffix.toString(true));
+      const message: Buffer = this.encodeBip32Path(suffix);
+      const isLastMessage: Boolean = ix >= derivationPathSuffixes.length - 1;
+      const signatureData = await this.transport.send(this.CLA, this.INS_SIGN_HASH, isLastMessage ? 0x81 : 0x01, 0x00, message);
+      resultMap.set(suffix.toString(true), signatureData.slice(0, -2));
     };
+    return resultMap;
   }
 
   /**
@@ -155,8 +151,8 @@ export default class Avalanche {
    *
    * {
    *   "version": "1.0.3",
-   *   "commit": "0000000000000000000000000000000000000000",
-   *   "name": "Avax"
+   *   "commit": "abcdcefg"
+   *   "name": "Avalanche"
    * }
    */
   async getAppConfiguration(): Promise<{
@@ -164,7 +160,7 @@ export default class Avalanche {
     commit: string,
     name: string,
   }> {
-    const data: Buffer = await this.transport.send(0x80, this.INS_VERSION, 0x00, 0x00);
+    const data: Buffer = await this.transport.send(this.CLA, this.INS_VERSION, 0x00, 0x00);
 
     const eatNBytes = function(input, n) {
       const out = input.slice(0, n);
@@ -184,7 +180,7 @@ export default class Avalanche {
     const [commitData, rest2] = eatWhile(rest1, c => c != 0);
     const [nameData, rest3] = eatWhile(rest2.slice(1), c => c != 0);
     if (rest3.toString("hex") != "009000") {
-      console.log("WARNING: Response data does not exactly match expected format for VERSION instruction");
+      this.logger("WARNING: Response data does not exactly match expected format for VERSION instruction");
     }
 
     return {
@@ -204,7 +200,25 @@ export default class Avalanche {
    * 79c46bc3
    */
   async getWalletId(): Promise<Buffer> {
-    const result = await this.transport.send(0x80, this.INS_GET_WALLET_ID, 0x00, 0x00);
+    const result = await this.transport.send(this.CLA, this.INS_GET_WALLET_ID, 0x00, 0x00);
     return result.slice(0, -2);
+  }
+
+
+  uInt8Buffer(uint8: int): Buffer {
+    let buff = Buffer.alloc(1);
+    buff.writeUInt8(uint8);
+    return buff;
+  }
+
+  uInt32BEBuffer(uint32: int): Buffer {
+    let buff = Buffer.alloc(4);
+    buff.writeUInt32BE(uint32);
+    return buff;
+  }
+
+  encodeBip32Path(path: BIPPath): Buffer {
+    const pathArr = path.toPathArray();
+    return Buffer.concat([this.uInt8Buffer(pathArr.length)].concat(pathArr.map(this.uInt32BEBuffer)));
   }
 }
